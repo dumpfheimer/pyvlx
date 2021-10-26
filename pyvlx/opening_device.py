c014de3fc03abe62f238ef7e02af9742a04ea8c5
Christopher Hoch <chris@hpc.at>
2021-10-26 10:28:54 +0200

    should have looked at the code...

--- pyvlx/opening_device.py
+++ pyvlx/opening_device.py
@@ -165,8 +165,8 @@ class Blind(OpeningDevice):
         self.target_position = TargetPosition()
 
     def get_send_orientation(self):
-        if self.target_position == TargetPosition.from_raw(TargetPosition.from_int(0)):
-            return TargetPosition.from_raw(TargetPosition.from_int(0xD300))
+        if self.target_position == Position(Position.from_int(0)):
+            return Position(Position.from_int(0xD300))
         else:
             return self.target_orientation
 
